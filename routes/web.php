31cd89d09eef8912f9f06a0bc71da917695d8da1
mahnafakmal <mahnafakmal@gmail.com>
2025-12-10 11:40:02 +0700

    personal

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,28 @@
 <?php
+// routes/web.php
 
 use App\Http\Controllers\PersonalColorController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
+// Route Personal Color
+Route::get('/personalcolor/basic/start', [PersonalColorController::class, 'basicStart'])
+    ->name('personalcolor.basic.start');
 
-Route::get('/', [PersonalColorController::class, 'welcome'])->name('welcome');
+Route::get('/personalcolor/basic/skin-tone', [PersonalColorController::class, 'skinTone'])
+    ->name('personalcolor.basic.skin-tone');
 
-Route::prefix('personal-color')->name('personal-color.')->group(function () {
-    
-    // Basic Flow Routes
-    Route::prefix('basic')->name('basic.')->group(function () {
-        Route::get('/start', [PersonalColorController::class, 'basicStart'])->name('start');
-        Route::post('/upload-photo', [PersonalColorController::class, 'uploadPhoto'])->name('upload-photo');
-        Route::get('/skin-tone', [PersonalColorController::class, 'skinTone'])->name('skin-tone');
-        Route::post('/skin-tone', [PersonalColorController::class, 'submitSkinTone'])->name('skin-tone.submit');
-        Route::get('/undertone/{step}', [PersonalColorController::class, 'undertone'])->name('undertone');
-        Route::post('/undertone', [PersonalColorController::class, 'submitUndertone'])->name('undertone.submit');
-        Route::get('/result', [PersonalColorController::class, 'result'])->name('result');
-        Route::post('/save-result', [PersonalColorController::class, 'saveResult'])->name('save-result');
-    });
-    
-    // Expert Flow Routes (untuk development selanjutnya)
-    Route::prefix('expert')->name('expert.')->group(function () {
-        Route::get('/start', [PersonalColorController::class, 'expertStart'])->name('start');
-        // ... tambahkan route expert lainnya
-    });
-});
+Route::post('/personalcolor/basic/skin-tone', [PersonalColorController::class, 'storeSkinTone'])
+    ->name('personalcolor.basic.store-skin-tone');
+
+Route::get('/personalcolor/basic/questions', [PersonalColorController::class, 'questions'])
+    ->name('personalcolor.basic.questions');
+
+Route::post('/personalcolor/basic/questions', [PersonalColorController::class, 'storeQuestions'])
+    ->name('personalcolor.basic.store-questions');
+
+Route::get('/personalcolor/basic/result', [PersonalColorController::class, 'result'])
+    ->name('personalcolor.basic.result'); 
+
+Route::post('/personalcolor/basic/reset', [PersonalColorController::class, 'resetSession'])
+    ->name('personalcolor.basic.reset');
+   
