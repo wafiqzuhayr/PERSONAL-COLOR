4ddb9cc77809a7cf0c61a87111d04e4242d4565a
inays <sykaninay@gmail.com>
2025-12-10 11:17:33 +0700

    edit

--- app/Http/Controllers/PersonalColorController.php
+++ app/Http/Controllers/PersonalColorController.php
@@ -1,85 +1,14 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use App\Services\ColorAnalysisService;
-use App\Models\PersonalColorResult;
-
-class PersonalColorController extends Controller
+public function basicStart()
 {
-    protected $service;
+    return view('personalcolor.basic.start');
+}
 
-    public function __construct(ColorAnalysisService $service)
-    {
-        $this->service = $service;
-    }
+public function skinTone()
+{
+    return view('personalcolor.basic.skin-tone');
+}
 
-    public function welcome()
-    {
-        return view('welcome');
-    }
-
-    // --- BASIC FLOW ---
-
-    public function basicStart()
-    {
-        return view('personalcolor.basic.start');
-    }
-
-    public function uploadPhoto(Request $request)
-    {
-        // proses upload (sementara kosong)
-        return redirect()->route('personal-color.basic.skin-tone');
-    }
-
-    public function skinTone()
-    {
-        return view('personalcolor.basic.skin-tone');
-    }
-
-    public function submitSkinTone(Request $request)
-    {
-        session(['skin_tone' => $request->skin_tone]);
-        return redirect()->route('personal-color.basic.undertone', ['step' => 1]);
-    }
-
-    public function undertone($step)
-    {
-        return view('personalcolor.basic.undertone', compact('step'));
-    }
-
-    public function submitUndertone(Request $request)
-    {
-        session(['undertone' => $request->undertone]);
-        return redirect()->route('personal-color.basic.result');
-    }
-
-    public function result()
-    {
-        $result = $this->service->generateResult(
-            session('skin_tone'),
-            session('undertone')
-        );
-
-        return view('personalcolor.basic.result', compact('result'));
-    }
-
-    public function saveResult(Request $request)
-    {
-        PersonalColorResult::create([
-            'skin' => session('skin_tone'),
-            'undertone' => session('undertone'),
-            'result' => $request->result
-        ]);
-
-        return redirect()->route('welcome')->with('success', 'Result saved!');
-    }
-
-    
-    // --- EXPERT FLOW (Belum dikembangkan) ---
-    public function expertStart()
-    {
-        return view('personalcolor.expert.start');
-    }
+public function result()
+{
+    return view('personalcolor.basic.result');
 }
--- resources/views/personal-color/start.blade.php
+++ resources/views/personal-color/start.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Personal Color Test</h1>
-    <a href="{{ route('personal-color.basic.skin-tone') }}">Mulai Tes</a>
+<div class="container">
+    <h1>Welcome to Personal Color Test 🎨</h1>
+
+    <a href="{{ route('personal-color.basic.skin-tone') }}" class="btn btn-primary">
+        Start Test
+    </a>
+</div>
 @endsection
